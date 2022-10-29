cebc7ef5b06313d6b329efe41320df2ad80330fd
rochmadnf <rochmadnf@gmail.com>
2022-10-29 10:16:23 +0800

    register page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/register",
+        destination: "/auth/register",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
+axios.defaults.baseURL = `${process.env.NEXT_PUBLIC_API_BACKEND}`;
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
